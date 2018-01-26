b76bf35f7fdf7be386e636e691636d7fd4b3159e
Gustav <sir.gustav.the.coder@gmail.com>
2018-01-27 00:35:30 +0100

    added basic snap classes

---
+++ Assets/Scripts/SnapPlug.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// plug is inserted into socket
+public class SnapPlug : MonoBehaviour {
+
+	// set in unity editor
+	public SnapPlug OtherSide;
+
+	// the socket this is connected to (only for reading)
+	// todo add some event handling?
+	public SnapSocket InSocket
+	{
+		get;
+		private set;
+	}
+
+	// called from snap socket
+	public void SetSocket(SnapSocket socket)
+	{
+		this.InSocket = socket;
+	}
+}
---
+++ Assets/Scripts/SnapSocket.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// socket requires plug
+public class SnapSocket : MonoBehaviour {
+	SnapPlug connected_plug_;
+
+	// the conencted plug
+	// TODO add some events?
+	public SnapPlug ConnectedPlug
+	{
+		get
+		{
+			return connected_plug_;
+		}
+
+		set
+		{
+			if(value && connected_plug_ != null)
+			{
+				connected_plug_.SetSocket(null);
+			}
+			connected_plug_ = value;
+			if(connected_plug_ != null)
+			{
+				connected_plug_.SetSocket(this);
+			}
+		}
+	}
+}
