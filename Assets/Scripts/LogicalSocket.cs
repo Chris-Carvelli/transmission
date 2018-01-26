1e23c624c625cd9afe2670cac91b005ce7651588
Gustav <sir.gustav.the.coder@gmail.com>
2018-01-27 00:59:17 +0100

    logical socket

---
+++ Assets/Scripts/LogicalSocket.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogicalSocket : MonoBehaviour {
+	public string Name
+	{
+		get
+		{
+			return this.gameObject.name;
+		}
+	}
+
+	public bool IsLightOn
+	{
+		// TODO: figure out how to we should handle lights.
+		// texture, point light, other?
+		get;
+		set;
+	}
+}
